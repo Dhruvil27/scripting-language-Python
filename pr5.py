860d1a01c46dacc61a92b37650e37e02a7ae7687
Dhruvil27 <103164455+Dhruvil27@users.noreply.github.com>
2022-11-15 12:38:20 +0530

    Add files via upload

---
+++ pr5.py
@@ -0,0 +1,17 @@
+num=int(input("Enter Number:"))
+total_sum=0
+
+
+for n  in range(num):
+
+    num=int(input("Enter Marks:"))
+    total_sum+=num
+avg=total_sum/2
+
+print("Your Average is:",avg)
+
+
+
+
+
+
