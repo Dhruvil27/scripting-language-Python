34f17ea4c4d5f57718fe9795f7b6ea8fc1711b22
Dhruvil27 <103164455+Dhruvil27@users.noreply.github.com>
2022-11-18 14:56:16 +0530

    Add files via upload

---
+++ Pr6_4.py
@@ -0,0 +1,13 @@
+string=input("Enter Strig:")
+c=0
+l=len(string)
+    
+
+for i in range(0,l):
+    print(string.count(string[i]))
+
+l=["H","E","L","L","o","I","N","D","I","A"]
+l.remove
+print(set(l))
+
+
