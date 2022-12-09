6ddecb3eb1aa44efc81a40b0857fc84bd56170b1
Dhruvil27 <103164455+Dhruvil27@users.noreply.github.com>
2022-12-09 14:59:21 +0530

    Add files via upload

--- Pr7_1.py
+++ Pr7_1.py
@@ -1,28 +1,15 @@
 
-print("--------Create A Tuple---------")
+print("Create A Tuple")
 T1=(11,12,13,34,876,468,30) #create a Truple 
 print(T1)
-
-
-
-
-print("-----Truple to List converstion-------")
+print("Truple to List converstion-------")
 T1=list(T1)
 print(T1)
-
-
-print("-------Removed item From a List--------")
-
+print("Removed item From a List--------")
 del T1[5]
 print(T1)
-
-
-print("--------Now Tuple Item Is--------")
+print("Now Tuple Item Is--------")
 print(T1)
-
-
-
 print("----- List to Tuple converstion-------")
-
 T1=tuple(T1)
 print(T1)
--- Pr7_2.py
+++ Pr7_2.py
@@ -2,40 +2,24 @@ s1={10,20,30}
 s2={20,30}
 print(s1)
 print(s2)
-
-
-
 print("--------Added Item in a SET------")
-
 s1.add(40)
 print(s1)
-
-
-
 print("--------Removed Item From a SET------")
-
 s1.discard(20)
 print(s1)
-
-
 print("---Union Operation---")
 s3=s1.union(s2)
 print(s3)
-
-
 print("---Intersaction  Operation---")
 s1.intersection(s2)
 print(s1)
-
 print("----Difference  Operation---")
-
 s1-s2
 s2-s1
 print(s1)
 print(s2)
-
 print("----symmeric Difference  Operation---")
-
 s1^s2
 s2^s1
 print(s1)
